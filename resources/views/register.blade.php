b6ef4674e090b86dde117180f96905c5bf71bb05
clem <c.laurencepro@gmail.com>
2021-01-28 19:10:44 +0100

    first commit

---
+++ resources/views/register.blade.php
@@ -0,0 +1,30 @@
+@include('home')
+
+
+<div class="form col-md-6">
+
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Username</label>
+  <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Username">
+</div>
+
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Email address</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="your email address">
+</div>
+
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Password</label>
+  <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="password">
+</div>
+
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Password confirmation</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Confirm your password">
+</div>
+
+<button class="btn btn-primary" type="submit">Inscription</button>
+
+
+</div>
+
